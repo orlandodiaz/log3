e8eacd6144639eaf99329d1a270c452070814c04
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2018-11-13 22:35:34 -0600

    Default level is now WARNING as expected

--- log3/log.py
+++ log3/log.py
@@ -22,10 +22,10 @@ def config():
     formatter = logging.Formatter(fmt, datefmt=date_fmt)
     stderr.setFormatter(fmt)
     logger.addHandler(stderr)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.WARNING)
 
     urllib_logger.addHandler(stderr)
-    urllib_logger.setLevel(logging.DEBUG)
+    urllib_logger.setLevel(logging.WARNING)
 
 
 config()
@@ -36,8 +36,13 @@ def log_to_file(log_path, log_urllib=False, limit=None):
     log_path = log_path
     file_handler = logging.FileHandler(log_path)
     if limit:
-        file_handler = RotatingFileHandler(log_path, mode='a', maxBytes=limit * 1024 * 1024,
-                                           backupCount=2, encoding=None, delay=0)
+        file_handler = RotatingFileHandler(
+            log_path,
+            mode='a',
+            maxBytes=limit * 1024 * 1024,
+            backupCount=2,
+            encoding=None,
+            delay=0)
     fmt = '[%(asctime)s %(filename)18s] %(levelname)-7s - %(message)7s'
     date_fmt = '%Y-%m-%d %H:%M:%S'
     formatter = logging.Formatter(fmt, datefmt=date_fmt)
